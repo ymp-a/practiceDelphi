947a0b630d2cbaabd7075441caebcc5fbf7f81f0
ybsyama <hiyakashi3@gmail.com>
2022-01-17 13:20:18 +0900

    Create Prohect

---
+++ Project/MNK001.dfm
@@ -0,0 +1,49 @@
+object MNK001Frm: TMNK001Frm
+  Left = 0
+  Top = 0
+  Caption = #12513#12491#12517#12540
+  ClientHeight = 242
+  ClientWidth = 262
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 48
+    Top = 8
+    Width = 150
+    Height = 25
+    Caption = #25285#24403#32773#29031#20250#30011#38754
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 48
+    Top = 48
+    Width = 150
+    Height = 25
+    Caption = #25285#24403#32773#12513#12531#12486
+    TabOrder = 1
+  end
+  object Button3: TButton
+    Left = 48
+    Top = 88
+    Width = 150
+    Height = 25
+    Caption = #35211#31309#29031#20250
+    TabOrder = 2
+  end
+  object Button4: TButton
+    Left = 48
+    Top = 128
+    Width = 150
+    Height = 25
+    Caption = #35211#31309#20837#21147
+    TabOrder = 3
+  end
+end
---
+++ Project/__recovery/DM2.pas
@@ -0,0 +1,171 @@
+unit DM2;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf,
+  FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt, FireDAC.UI.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Phys, FireDAC.VCLUI.Wait,
+  Data.DB, FireDAC.Comp.Client, FireDAC.Comp.DataSet, FireDAC.Phys.MSSQL,
+  FireDAC.Phys.MSSQLDef, Datasnap.Provider, Datasnap.DBClient;
+
+type
+  TDataModule2 = class(TDataModule)
+    FDQueryLogin: TFDQuery;
+    FDConnection1: TFDConnection;
+    DataSource1: TDataSource;
+    FDQueryLoginTNTNCD: TIntegerField;
+    FDQueryLoginTNBKCD: TIntegerField;
+    FDQueryLoginTNPASS: TVarBytesField;
+    FDQueryLoginTNNAME: TStringField;
+    FDQueryLoginTNKGKB: TStringField;
+    FDQueryLoginTNSTKB: TStringField;
+    FDQueryLoginTNPWLA: TDateField;
+    FDQueryLoginTNCRDT: TDateField;
+    FDQueryLoginTNCRTM: TTimeField;
+    FDQueryLoginTNCRPG: TStringField;
+    FDQueryLoginTNCRWS: TStringField;
+    FDQueryLoginTNCRUS: TStringField;
+    FDQueryLoginTNUPDT: TDateField;
+    FDQueryLoginTNUPTM: TTimeField;
+    FDQueryLoginTNUPPG: TStringField;
+    FDQueryLoginTNUPWS: TStringField;
+    FDQueryLoginTNUPUS: TStringField;
+    FDQueryLoginTNJTCD: TStringField;
+    FDQueryLogin2: TFDQuery;
+    IntegerField1: TIntegerField;
+    IntegerField2: TIntegerField;
+    VarBytesField1: TVarBytesField;
+    StringField1: TStringField;
+    StringField2: TStringField;
+    StringField3: TStringField;
+    DateField1: TDateField;
+    DateField2: TDateField;
+    TimeField1: TTimeField;
+    StringField4: TStringField;
+    StringField5: TStringField;
+    StringField6: TStringField;
+    DateField3: TDateField;
+    TimeField2: TTimeField;
+    StringField7: TStringField;
+    StringField8: TStringField;
+    StringField9: TStringField;
+    StringField10: TStringField;
+    DataSource2: TDataSource;
+    FDQueryLogin3: TFDQuery;
+    DataSource3: TDataSource;
+    ClientDataSet1: TClientDataSet;
+    ClientDataSet2: TClientDataSet;
+    ClientDataSet3: TClientDataSet;
+    DataSetProvider1: TDataSetProvider;
+    DataSetProvider2: TDataSetProvider;
+    DataSetProvider3: TDataSetProvider;
+    ClientDataSet1TNTNCD: TIntegerField;
+    ClientDataSet1TNBKCD: TIntegerField;
+    ClientDataSet1TNPASS: TVarBytesField;
+    ClientDataSet1TNNAME: TStringField;
+    ClientDataSet1TNKGKB: TStringField;
+    ClientDataSet1TNSTKB: TStringField;
+    ClientDataSet1TNPWLA: TDateField;
+    ClientDataSet1TNCRDT: TDateField;
+    ClientDataSet1TNCRTM: TTimeField;
+    ClientDataSet1TNCRPG: TStringField;
+    ClientDataSet1TNCRWS: TStringField;
+    ClientDataSet1TNCRUS: TStringField;
+    ClientDataSet1TNUPDT: TDateField;
+    ClientDataSet1TNUPTM: TTimeField;
+    ClientDataSet1TNUPPG: TStringField;
+    ClientDataSet1TNUPWS: TStringField;
+    ClientDataSet1TNUPUS: TStringField;
+    ClientDataSet1TNJTCD: TStringField;
+  private
+    { Private 宣言 }
+  public
+    { Public 宣言 }
+    //担当者検索
+    procedure OpenTNData(TNCD,reNAME: String);
+  end;
+
+var
+  DataModule2: TDataModule2;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+uses functions, Utilybs;
+
+{$R *.dfm}
+
+
+procedure TDataModule2.OpenTNData(TNCD,reNAME: String);
+var
+  andFlg:boolean; //入力フラグを設定
+
+begin
+  //フラグ初期化
+  andFlg:=false;
+
+  with ClientDataSet1 do
+  begin
+    //データセットを閉じる
+    Active := False;
+    //Close;
+
+    with DataModule2.FDQueryLogin do
+    begin
+      Close;
+      //SQL文開始
+      //SQL文初期化
+      SQL.Clear;
+      //ここからSQL文↓
+      SQL.Add(' SELECT * FROM TNMMSP  ');
+
+      //担当者CD入力時の処理
+      if TNCD<>'' then
+      begin
+        //TNTNCDに入力した担当者CDを代入する
+        SQL.Add(' WHERE TNTNCD >= :TNCD ');
+        //入力した担当者CDを'TNCD'に代入する
+        ParamByName('TNCD').AsString:=TNCD;
+        //入力時フラグオン
+        andFlg:=true;
+      end;
+
+      //担当者名入力時の処理
+      if reNAME<>'' then
+      begin
+        //担当者CD入力有無でANDかWHEREに条件分岐する
+        if andFlg=true then
+         SQL.Add(' AND ')
+         else SQL.Add(' WHERE ');
+
+        //TNNAME LIKEに%入力名%をSQLStringに反映する
+        SQL.Add(' TNNAME LIKE :NAME ');
+        //部分一致の入力名を'NAME'へ代入する
+        ParamByName('NAME').AsWideString :='%' +reNAME+ '%';
+        //入力時フラグオン
+        andFlg:=true;
+      end;
+
+      //昇順
+//      SQL.Add(' ORDER BY TNTNCD ');
+      Open;
+    end;
+
+
+    //データセットを開く
+    Active := True;
+    Open;
+    //対象データが存在しない場合、データセットを閉じて終了
+    if Eof and Bof then
+    begin
+    Active := False;
+    raise Exception.Create('対象データが存在しません');
+    end;
+  end;
+
+end;
+
+end.
